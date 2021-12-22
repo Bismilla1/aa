6eac55bfcb6ebc6f6f91d7135a12667454bdebc9
bismillas <bismillask1@gmail.com>
2021-12-22 12:23:54 +0530

    Added new files

---
+++ maxminarray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = {  9,7, 78,1 } ;
+        int  i,min, max;
+        int n=arr.Length;
+        max = arr[0];
+        min = arr[0];
+        for (i = 1; i < n; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+               
+            }
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        Console.WriteLine("Maximum element =   " + max);
+        Console.Write("Minimum element =   " +min);
+        Console.ReadKey();
+    }
+
+
+        
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
